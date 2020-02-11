dbd4057b01803f2125f0c6f1fb684331724b3225
fedPass <federica.passanante@gmail.com>
2020-02-11 15:32:51 +0100

    header, ancora da finire lo style

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,18 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <a class="navbar-brand" href="/">
+          <img src="https://www.boolean.careers/images/common/logo.png" alt="Boolean Careers"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-boolean nav-item nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-boolean nav-item nav-link" href="#">Corso</a>
+          <a class="nav-boolean nav-item nav-link" href="#">Dopo il corso</a>
+          <a class="nav-boolean nav-item nav-link" href="#">Lezione gratuita</a>
+          <a class="nav-boolean nav-item nav-link btn btn-success" href="#">Candidati ora</a>
+        </div>
+      </div>
+    </nav>
+</header>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,9 +4,12 @@
         <title>Boolean Careers</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css?family=Rubik&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
+        @include('layouts.header')
         @yield('content')
+        @include('layouts.footer')
     </body>
 </html>
