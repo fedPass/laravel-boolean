da0f280b33c32e76f154ae15dcce1b4f1dfd4e0d
fedPass <federica.passanante@gmail.com>
2020-02-11 12:37:39 +0100

    create routes e file blade iniziali

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="it" dir="ltr">
+    <head>
+        <title>Boolean Careers</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <h1>Diventa <strong>sviluppatore web</strong></h1>
+    </body>
+</html>
